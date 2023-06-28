938e01afb2fba9997f14044446145cfea7f2be7a
SEOKKAMONI <sj01048260279@gmail.com>
2023-06-29 01:42:02 +0900

    SET :: 초기 세팅

---
+++ src/components/common/Provider/index.tsx
@@ -0,0 +1,17 @@
+import GlobalStyle from "@/styles/global";
+import { ReactNode } from "react";
+
+interface PropsType {
+  children: ReactNode;
+}
+
+const Provider = ({ children }: PropsType) => {
+  return (
+    <>
+      <GlobalStyle />
+      {children}
+    </>
+  );
+};
+
+export default Provider;
